3d2945f8bc78fdfd5fb0b4440dc6269dbd577c81
Egor Zotov <egor_zotov@list.ru>
2020-04-18 21:07:55 +0300

    Simple echoe_bot v.1

---
+++ config.py/config.py
@@ -0,0 +1,8 @@
+TOKEN = '1211344336:AAHYy-WHP7qulhGpOAYtsq1it4OEsIPqJPs' #From BotFather
+
+
+
+
+
+
+
---
+++ shittycode/text.py
@@ -0,0 +1,7 @@
+import random
+
+x = [1, 1, 1, 1, 1, 1, 1, 1]
+for n in range(8):
+    x[n] = random.randint(1, 101)
+print(x)
+
