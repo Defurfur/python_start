59655e55ad47570b8a2b62772c844215e26b5b52
Egor Zotov <egor_zotov@list.ru>
2020-04-19 20:45:42 +0300

    Simple echoe_bot v1.2

--- config.py/bot.py
+++ config.py/bot.py
@@ -3,8 +3,15 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
+
 @bot.message_handler(content_types=['text'])
 def echoe_func(message):
     bot.send_message(message.chat.id, message.text)
+
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    bot.send_sticker(message.chat.id, message.sticker.file_id)
+
 
 bot.polling(none_stop=True)
