51a89987dd2d96b7628c163a830111664b9db278
Egor Zotov <egor_zotov@list.ru>
2020-04-19 23:30:25 +0300

    Simple echoe_bot v1.3

--- config.py/bot.py
+++ config.py/bot.py
@@ -1,17 +1,35 @@
 import telebot
 import config
 
-bot = telebot.TeleBot(config.TOKEN)
 
+bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def echoe_func(message):
     bot.send_message(message.chat.id, message.text)
 
-
 @bot.message_handler(content_types=['sticker'])
-def sticker_id(message):
+def sti_func(message):
     bot.send_sticker(message.chat.id, message.sticker.file_id)
 
+@bot.message_handler(content_types=['audio'])
+def audio_func(message):
+    bot.send_audio(message.chat.id, message.audio.file_id)
+
+@bot.message_handler(content_types=['photo'])
+def ph_func(message):
+   bot.send_photo(message.chat.id, message.photo[0].file_id) #Еле допер, что там был список O_o
+
+@bot.message_handler(content_types=['document'])
+def dcmt_dunc(message):
+    bot.send_document(message.chat.id, message.document.file_id)
+
+@bot.message_handler(content_types=['video'])
+def vid_dunc(message):
+    bot.send_video(message.chat.id, message.video.file_id)
+
+@bot.message_handler(content_types=['voice'])
+def voi_func(message):
+    bot.send_voice(message.chat.id, message.voice.file_id)
 
 bot.polling(none_stop=True)
--- config.py/config.py
+++ config.py/config.py
@@ -1,4 +1,4 @@
-TOKEN = '1211344336:AAHYy-WHP7qulhGpOAYtsq1it4OEsIPqJPs' #From BotFather
+
 
 
 
